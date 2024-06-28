408fd34b1512b2b16149071cef101e289c82f131
Bogdan Protopopescu <bprotopopescu09@gmail.com>
2024-06-28 14:03:03 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -34,8 +34,8 @@ builder.Services.AddAuthorization(options =>
 builder.Services.AddAuthentication()
     .AddSpotify(options =>
     {
-        options.ClientId = "d2d70f2bfbfb4f2594339fc8916b16a1";
-        options.ClientSecret = "3894a81b65754b039d84817bb52ea144";
+        options.ClientId = "CLIENT_ID";
+        options.ClientSecret = "CLIENT_SECRET";
         options.CallbackPath = "/callback";
         options.Events.OnRemoteFailure = (Context) =>
         {
